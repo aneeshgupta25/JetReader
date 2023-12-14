38747bbd912b5e76dcda8e2a627152b7afb2d389
aneeshgupta25 <aneeshgupta2512@gmail.com>
2023-12-14 11:47:56 +0530

    Data Model Classes

---
+++ app/src/main/java/com/example/jetreader/model/volume/Epub.kt
@@ -0,0 +1,6 @@
+package com.example.jetreader.model.volume
+
+data class Epub(
+    val acsTokenLink: String,
+    val isAvailable: Boolean
+)
---
+++ app/src/main/java/com/example/jetreader/model/volume/SearchInfo.kt
@@ -0,0 +1,5 @@
+package com.example.jetreader.model.volume
+
+data class SearchInfo(
+    val textSnippet: String
+)
